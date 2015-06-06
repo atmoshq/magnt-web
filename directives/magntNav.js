25beb47f820f13530124a0daa9e0006f1a92627a
Ved Petkar <vedpetkar@gmail.com>
2015-06-06 00:07:15 -0400

    brand new welcome-view

--- directives/magntNav.js
+++
@@ -1,8 +0,0 @@
-var magntDirective = angular.module('magntDirective', []);
-
-magntDirective.directive('magntNav', function() {
-  return {
-    restrict: 'E',
-    templateUrl: 'directives/views/magnt-nav.html'
-  };
-});
--- directives/master-directives.js
+++ directives/master-directives.js
@@ -1,5 +1,11 @@
 var magntDirective = angular.module('magntDirective', []);
 
+magntDirective.directive('magntNav', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/views/magnt-nav.html'
+  };
+});
 magntDirective.directive('magntSide', function() {
   return {
     restrict: 'E',
