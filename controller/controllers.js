f049f6ed27902aad8ba7c434d1acc78f1a3c4d83
Ved Petkar <vedpetkar@gmail.com>
2015-06-09 00:43:22 -0400

    remove redundant data binding

--- controller/controllers.js
+++ controller/controllers.js
@@ -107,14 +107,6 @@ magntControllers.controller('AskQuestion', ['$scope', '$http', '$routeParams', '
       success(function(data, status, headers, config){
         if(status == 200) {
           $scope.askResult = "Got your question.";
-          console.log(data.id);
-          apiQuestions.singleQuestion(data.id).then(function(d) {
-            $timeout(function(){
-              $scope.$apply(function(){
-                $scope.questionList.push(d.data);
-              });
-            }, 0);
-          });
         }
         else {
           $scope.askResult = "There was an error submitting your question";
@@ -122,9 +114,6 @@ magntControllers.controller('AskQuestion', ['$scope', '$http', '$routeParams', '
       }).
       error(function(data, status, headers, config){
         $scope.askResult = "There was an error submitting your question";
-      });
-      apiQuestions.getQuestions($scope.magnetId).then(function(d) {
-        $scope.questionList = d.data;
       });
   }
 }]);
